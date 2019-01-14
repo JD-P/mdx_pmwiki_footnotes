cabc0053857528c42f881630c78ab7d6265cd640
John David Pressman <jd@jdpressman.com>
2019-01-13 22:18:55 -0800

    Change setup.py to include package declarations

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="mdx-pmwiki-footnotes",
     version="0.1",
+    packages=find_packages(),
     install_requires=['Markdown>=2.6.11'],
     author="John David Pressman",
     author_email="jd@jdpressman.com",
